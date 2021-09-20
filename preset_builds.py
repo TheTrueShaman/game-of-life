0805c10f95e35d882772cb8967303d5847ded3bb
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2021-09-20 18:13:57 +0200

    Change to accommodate alive being a set.

--- preset_builds.py
+++ preset_builds.py
@@ -1,21 +1,21 @@
 def neglider(x, y, alive):
     build = [(x,y), (x-1,y-1), (x-1,y+1), (x,y+1), (x+1,y)]
-    alive.extend(build)
+    alive.update(build)
 
 
 def nwglider(x, y, alive):
     build = [(x,y), (x+1,y-1), (x+1,y+1), (x,y+1),(x-1,y)]
-    alive.extend(build)
+    alive.update(build)
 
 
 def seglider(x, y, alive):
     build = [(x,y), (x-1,y+1), (x-1,y-1), (x,y-1), (x+1,y)]
-    alive.extend(build)
+    alive.update(build)
 
 
 def swglider(x, y, alive):
     build = [(x,y), (x+1,y+1), (x+1,y-1), (x,y-1),(x-1,y)]
-    alive.extend(build)
+    alive.update(build)
 
 
 def glider_gun(x,y, alive):
@@ -24,6 +24,4 @@ def glider_gun(x,y, alive):
              (x-1, y), (x+3, y), (x+4, y), (x-6, y+1), (x-2, y+1), (x+3, y+1), (x+4, y+1), (x+17, y+1), (x+18, y+1),
              (x-5, y+2), (x-4, y+2), (x+3, y+2), (x+4, y+2), (x+17, y+2), (x+18, y+2), (x+5, y+3), (x+7, y+3),
              (x+7, y+4)]
-    alive.extend(build)
-
-
+    alive.update(build)
